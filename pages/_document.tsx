63146be5f6ab0d7f94e6508be641eff2da3242c3
Praveen Thirumurugan <mail@praveent.com>
2025-01-19 16:16:23 +0530

    feat: enhance document metadata and update favicon assets
    - Added comprehensive metadata including title and Open Graph tags to `_document.tsx` for improved SEO and social sharing.
    - Updated favicon assets with new images and manifest for better branding and user experience.
    - Removed unused SVG files to clean up the public directory.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Prepity - Your AI-Powered MCQ Questions Generator</title>
+        <meta name="description" content="Prepity helps to generate MCQ questions for exams, interviews and for fun with the help of AI. It can create any kind of questions with ease and in seconds." />
+        <meta property="og:title" content="Prepity - Your AI-Powered MCQ Questions Generator" />
+        <meta property="og:description" content="Prepity helps to generate MCQ questions for exams, interviews and for fun with the help of AI. It can create any kind of questions with ease and in seconds." />
+        <meta property="og:image" content="https://www.prepity.com/images/common/prepity_og.png" />
+        <meta property="og:url" content="https://www.prepity.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@praveentcom" />
+        <meta name="twitter:title" content="Prepity - Your AI-Powered MCQ Questions Generator" />
+        <meta name="twitter:description" content="Prepity helps to generate MCQ questions for exams, interviews and for fun with the help of AI. It can create any kind of questions with ease and in seconds." />
+        <meta name="twitter:image" content="https://www.prepity.com/images/common/prepity_og.png" />
+        <meta name="apple-mobile-web-app-title" content="Prepity" />
+        <link rel="manifest" href="/images/favicon/manifest.json" />
+      </Head>
       <body className="antialiased h-full bg-muted">
         <Main />
         <NextScript />
