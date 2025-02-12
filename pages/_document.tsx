28b76724685528b44b59e5e7bdc78935bdf3352c
Praveen Thirumurugan <mail@praveent.com>
2025-02-13 02:06:44 +0530

    refactor: Use environment variable for Open Graph and Twitter meta image URLs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,13 +8,13 @@ export default function Document() {
         <meta name="description" content="Prepity helps to generate MCQ questions for exams, interviews and for fun with the help of AI. It can create any kind of questions with ease and in seconds." />
         <meta property="og:title" content="Prepity | AI-Powered MCQ Questions Generator" />
         <meta property="og:description" content="Prepity helps to generate MCQ questions for exams, interviews and for fun with the help of AI. It can create any kind of questions with ease and in seconds." />
-        <meta property="og:image" content="https://www.prepity.com/images/common/prepity_og.png" />
-        <meta property="og:url" content="https://www.prepity.com" />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/images/common/prepity_og.png`} />
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_BASE_URL}`} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@praveentcom" />
         <meta name="twitter:title" content="Prepity | AI-Powered MCQ Questions Generator" />
         <meta name="twitter:description" content="Prepity helps to generate MCQ questions for exams, interviews and for fun with the help of AI. It can create any kind of questions with ease and in seconds." />
-        <meta name="twitter:image" content="https://www.prepity.com/images/common/prepity_og.png" />
+        <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/images/common/prepity_og.png`} />
         <meta name="apple-mobile-web-app-title" content="Prepity" />
         <link rel="manifest" href="/images/favicon/manifest.json" />
         {process.env.NEXT_PUBLIC_UMAMI_HOST && process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
